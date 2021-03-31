26d82413efc4d2fbe8ac5b270239805f9fb594b8
Jake Wood <jakealistairwood@gmail.com>
2021-03-31 13:39:28 +0100

    jobCard styling in situ

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -5,15 +5,15 @@ const JobCard = ({ job }) => {
     return (
         <div className={styles.jobCard}>
             <div className={styles.jobCard__logo}>
-                <img src="" alt=""/>
+                <img src={job.company_logo} alt=""/>
             </div>
             <div className={styles.jobCard__body}>
-                <p className={styles.jobCard__postDate}>5 hrs ago</p>
+                <p className={styles.jobCard__postDate}>5h ago . Full Time</p>
                 <div className={styles.jobCard__description}>
                     <h3>{job.title}</h3>
-                    <p className={styles.jobCard__jobCompany}>company</p>
+                    <p className={styles.jobCard__jobCompany}>{job.company}</p>
                 </div>
-                <p className="jobCard__location">location</p>
+                <p className={styles.jobCard__location}>{job.location}</p>
             </div>
         </div>
     )
