a7b74f56d2310f5c0d24f90c795c43bfa23c5f9b
Jake Wood <jakealistairwood@gmail.com>
2021-03-31 10:08:13 +0100

    project clear-out

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="app">
+      {/* Navigation */}
+      {/* Job Library */}  
+    </div>
+  )
+}
+
+export default App;
