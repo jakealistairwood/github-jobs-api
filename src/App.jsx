94c3bc73ddd6a0ad6feaae44621af51675be3778
Jake Wood <jakealistairwood@gmail.com>
2021-03-31 11:08:09 +0100

    axios request working - jobs stored in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 const App = () => {
+  const [ jobs, setJobs ] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://cors.bridged.cc/https://jobs.github.com/positions.json';
+    const fetchData = async () => {
+      const request = await axios.get(url);
+      setJobs(request.data);
+    }
+    fetchData();
+  },[]);
+
   return (
     <div className="app">
       {/* Navigation */}
