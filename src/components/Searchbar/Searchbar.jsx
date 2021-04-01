73d66fe17276bd70cc6384cb89d8ddd30969f283
Jake Wood <jakealistairwood@gmail.com>
2021-04-01 10:49:54 +0100

    searchbar in situ - styles and theme all working - now to add api search functionality

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
 import styles from './Searchbar.module.scss';
+import SearchIcon from '../../img/desktop/icon-search.svg';
+import FilterIcon from '../../img/desktop/icon-location.svg';
+import styled from 'styled-components';
+
+const StyledSearchBar = styled.form`
+    background-color: ${props => props.theme.cardBg}
+`
+
+const StyledLabel = styled.label`
+    color: ${props => props.theme.fullTimeLabel}
+`
+
+const StyledDiv = styled.div`
+    border-right: 1px solid ${props => props.theme.borderRight}
+`
 
 const Searchbar = () => {
     return (
-        <div className={styles.search}>
-            
-        </div>
+        <StyledSearchBar className={styles.search}>
+            <StyledDiv className={styles.search__filterOne}>
+                <img src={SearchIcon} alt="magnifying-glass-icon" />
+                <label htmlFor="searchByJob"></label>
+                <input 
+                    type="text" 
+                    id="searchByJob" 
+                    placeholder="Filter by title, companies, expertise..."
+                />
+            </StyledDiv>    
+            <StyledDiv className={styles.search__filterTwo}>
+                <img src={FilterIcon} alt="location-pin-icon"/>
+                <label htmlFor="searchByLocation"></label>
+                <input 
+                    type="text" 
+                    id="searchByLocation"
+                    placeholder="Filter by location..."
+                />
+            </StyledDiv>    
+            <div className={styles.search__filterThree}>
+                <div>
+                    <input 
+                        type="checkbox" 
+                        id="filterFullTimeRoles"
+                    />
+                    <StyledLabel htmlFor="filterFullTimeRoles">Full Time Only</StyledLabel>
+                </div>
+                <button type="submit" className={styles.primaryBtn}>Search</button>
+            </div>    
+        </StyledSearchBar>
     )
 }
 
--- src/themes.js
+++ src/themes.js
@@ -4,14 +4,22 @@ export const lightMode = {
     body: '#F4F6F8',
     fontColor: '#6E8098',
     cardBg: '#FFFFFF',
-    h3: '#19202D'
+    h3: '#19202D',
+    input: '#FFFFFF',
+    inputText: '#19202D',
+    fullTimeLabel: '#19202D',
+    borderRight: '#E2E6EA'
 }
 
 export const darkMode = {
     body: '#121721',
     fontColor: '#6E8098',
     cardBg: '#19202D',
-    h3: '#FFFFFF'
+    h3: '#FFFFFF',
+    input: '#19202D',
+    inputText: '#FFFFFF',
+    fullTimeLabel: '#FFFFFF',
+    borderRight: '#6E8098'
 }
 
 export const GlobalStyles = createGlobalStyle`
@@ -21,5 +29,10 @@ export const GlobalStyles = createGlobalStyle`
 
     h3 {
         color: ${props => props.theme.h3}
+    }
+
+    input[type="text"] {
+        background-color: ${props => props.theme.input};
+        color: ${props => props.theme.inputText};
     }
 `
